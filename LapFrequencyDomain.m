db164ea0d88c9189637018e3b4596dd7ed27e0ae
shiannn <b05502087@ntu.edu.tw>
2019-10-26 17:03:16 +0800

    Commit LapFrequency.m LapSpatial.m Finish the experiment of filter in spatial domain and frequency domain

---
+++ LapFrequencyDomain.m
@@ -0,0 +1,15 @@
+img = imread("barbara.tif");
+td = 2;
+lr = 2;
+[m,n] = size(img);
+NewImg = zeros(m,n);
+Paddledimg = [zeros(size(img,1),lr),img,zeros(size(img,1),lr)];
+Paddledimg = [zeros(td,size(Paddledimg,2));Paddledimg;zeros(td,size(Paddledimg,2))];
+fftImage = fft2(double(img),size(Paddledimg,1),size(Paddledimg,2));
+H1 = [1,1,1;1,-8,1;1,1,1];
+H2 = [0,-1,0;-1,4,-1;0,-1,0];
+fftH=fft2(double(H1),size(Paddledimg,1),size(Paddledimg,2));
+G = fftImage .* fftH;
+g=real(ifft2(G));
+figure;
+imshow(g);
